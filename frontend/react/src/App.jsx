351b42b08cb63474502d6f4e96ecaf1d1e489f65
hosi <blackpussy@duck.com>
2025-06-27 14:40:46 +0330

    webpage is now with tailwind insted of chakra (also added the ui elements for tailwind)

--- frontend/react/src/App.jsx
+++ frontend/react/src/App.jsx
@@ -1,7 +1,19 @@
+import SidebarDark from "./components/my-components/SidebarDark"
 
-const App = () => {
-    return (
-        "hey bitch"
+function App() {
+
+  return (
+    <div className="flex h-screen w-screen">
+      <div className="w-80 bg-gray-900">
+        <SidebarDark />
+      </div>
+
+      <main className="h-full w-full bg-white text-black p-6 ">
+        <h1 className="text-2xl font-bold">Full-Width Main Content</h1>
+        <p>This area takes the full screen width.</p>
+      </main>
+    </div>
+
   )
 }
 
